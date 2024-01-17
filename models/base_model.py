a2e696de6926458829a49fadd1da03f24ef9c4f8
Lewis <lewismomanyi34@gmail.com>
2024-01-17 16:43:15 +0300

    Proceeding with the base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,12 @@
 A class BaseModel that defines all common attributes/methods for other classes:
 """
 
-from uuid import uuid4
+from uuid import uuid
 from datetime import datetime
 
 class BaseModel:
     """
-    A documentation of Public Instance Attributes:
+    A  Public Instance Attributes:
     """
     
     def __init__(self, name):
@@ -28,3 +28,26 @@ class BaseModel:
         # Updating the updated_at timestamp whenever the object is modified
         self.updated_at = datetime.utcnow()
 
+    def __str__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+    
+    """
+     A documentation of Public instance methods:
+    """
+
+    def __init__(self, name):
+        # Public instance method
+        self.name = name
+
+    def save(self):
+        # Public instance method to update the public instance attribute updated_at with the current datetime
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        # A pubic instance method to return a dictionary containing all keys/values of __dict__ of the instance
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
