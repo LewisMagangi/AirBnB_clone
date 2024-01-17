6ec2b8c06a8b283eac7239ffd3336e435e3017ad
Lewis <lewismomanyi34@gmail.com>
2024-01-17 12:22:11 +0300

    moving on with the base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,27 @@
 A class BaseModel that defines all common attributes/methods for other classes:
 """
 
+import uuid
+import datetime
+
 class BaseModel:
     """
     A documentation of Public Instance Attributes:
     """
+    
+    def __init__(self, name):
+        # Public instance attribute
+        self.name = name
+
+        # Assigning a unique id using uuid.uuid4() and converting it to a string
+        self.id = str(uuid.uuid4())
+
+        # Assigning created_at and updated_at with the current datetime
+        current_datetime = datetime.utcnow()
+        self.created_at = current_datetime
+        self.updated_at = current_datetime
+
+    def update_timestamp(self):
+        # Updating the updated_at timestamp whenever the object is modified
+        self.updated_at = datetime.utcnow()
+
